48d39d1cfccf38c1f007079d5d9c77c792ce97d3
Belma Huseinoska <bhuseinoska01@gmail.com>
2023-08-08 18:29:34 +0200

    Added Deno Migration for ToDo App
    Changes Made:
    - Created a new Deno application structure.
    - Migrated server logic from Express to Oak framework for Deno.
    - Updated middleware and request handling to work with Deno syntax.
    - Used EJS templating for rendering views.
    - Implemented task addition and deletion functionality in Deno.
    - Ensured data consistency and correct type handling.

---
+++ Migration/deps.ts
@@ -0,0 +1,11 @@
+// deps.ts
+export {
+    Application,
+    Router,
+    Context,
+    send,
+    Status,
+} from "https://deno.land/x/oak/mod.ts";
+export {
+    renderFileToString,
+} from "https://deno.land/x/dejs/mod.ts";
---
+++ Migration/server.ts
@@ -0,0 +1,64 @@
+import { Application, Context, Router, send } from "./deps.ts";
+import { renderFileToString } from "./deps.ts";
+
+interface Task {
+    name: string;
+    date: Date;
+    description: string;
+}
+
+const app = new Application();
+const router = new Router();
+
+let tasks: Task[] = [];
+
+
+router.get("/", async (ctx: Context) => {
+    const today = new Date().toDateString();
+    const body = await renderFileToString("./views/index.ejs", { tasks, today, index: ctx.params.index });
+    ctx.response.body = body;
+});
+
+
+router.post("/add", async (ctx: Context) => {
+    const formData = await ctx.request.body().value;
+    const name = formData.get("name");
+    const taskDate = formData.get("taskDate");
+    const description = formData.get("description");
+
+    console.log("Received form data:", name, taskDate, description);
+
+        if (name && taskDate && description) {
+            const date = new Date(taskDate);
+            const task: Task = { name, date, description };
+            tasks.push(task);
+        }
+        ctx.response.redirect("/");
+    });
+router.post("/delete", async (ctx: Context) => {
+    const formData = await ctx.request.body().value;
+    const taskIndex = formData.get("taskIndex");
+    console.log("Received delete request for taskIndex:", taskIndex);
+
+    if (taskIndex) {
+        const index = parseInt(taskIndex, 10);
+        if (index >= 0 && index < tasks.length) {
+            tasks.splice(index, 1);
+            console.log("Task deleted:", tasks);
+        }
+    }
+    ctx.response.redirect("/");
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async (ctx) => {
+    await send(ctx, ctx.request.url.pathname, {
+        root: `${Deno.cwd()}/static`,
+        index: "index.html",
+    });
+});
+
+console.log("Server is running on http://localhost:8000");
+await app.listen({ port: 8000 });
